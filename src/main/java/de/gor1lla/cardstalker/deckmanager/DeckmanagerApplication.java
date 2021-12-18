6843edbe40efb44b33fac9927edcc4084aa36101
Deldrio <kloetzer.2508@web.de>
2021-12-18 20:33:33 +0100

    Start Project

---
+++ src/main/java/de/gor1lla/cardstalker/deckmanager/DeckmanagerApplication.java
@@ -0,0 +1,13 @@
+package de.gor1lla.cardstalker.deckmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeckmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeckmanagerApplication.class, args);
+	}
+
+}
