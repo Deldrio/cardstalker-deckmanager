bc189fa206e64182ac1d912cbfe876d95c4db499
Deldrio <kloetzer.2508@web.de>
2021-12-19 17:14:35 +0100

    basic Web setup

--- src/main/java/de/gor1lla/cardstalker/deckmanager/web/DeckWebController.java
+++ src/main/java/de/gor1lla/cardstalker/deckmanager/web/DeckWebController.java
@@ -5,12 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
-public class LandingPageWebController {
+@RequestMapping("/decks")
+public class DeckWebController {
 	
-	@GetMapping("/")
-	public String getLandingPage() {
-		return "index";
+	@GetMapping
+	public String getDeckSite() {
+		return "deckeditsite";
 	}
-
 }
